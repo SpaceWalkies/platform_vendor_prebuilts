df5a955aff0506ed886a299239b6671c12741054
isouzz <enzoo.site@gmail.com>
2020-07-02 16:15:05 +0100

    prebuilts: Keep only lawnchair

--- packages.mk
+++ packages.mk
@@ -1,16 +1,4 @@
-# Long screenshot
-PRODUCT_PACKAGES += \
-    Longshot
-
-# Extra packages
-ifeq ($(DERP_BUILD_ZIP_TYPE), VANILLA)
-PRODUCT_PACKAGES += \
-    Firefox-lite
-endif
-
 # Lawnchair
-ifeq ($(LAWNCHAIR_OPTOUT),)
 PRODUCT_PACKAGES += \
     Lawnchair \
     Lawnfeed
-endif
